2dd6769673603f89a679457db89e1a3585c310a2
Alexander <prepare4future@yahoo.com>
2019-10-10 10:39:55 +0400

    Добавлен метод итерации

---
+++ iteration.js
@@ -0,0 +1,34 @@
+function iteration() {
+    let matrix = parseMatrix();
+    let res = [];
+    let num = prompt("Введите количество итераций", 10);
+
+    for(let i = 0; i < matrix.length; i++) {
+        res[i] = 0;
+    }
+
+    for(let i = 0; i < Number(num); i++) {
+        res = iterationMain(matrix, res);
+    }
+
+    resulter(res, 'Метод итерации');
+}
+
+function iterationMain(matrix, res) {
+
+    let sum = 0;
+    let result = [];
+
+    for(let i = 0; i < matrix.length; i++) {
+        sum = 0;
+        for(let j = 0; j < matrix.length; j++) {
+            if(i !== j) {
+                sum += matrix[i][j] * res[j];
+            }
+        }
+        result[i] = (matrix[i][matrix.length] - sum) / matrix[i][i];
+    }
+
+    return result;
+
+}
