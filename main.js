8211bc1bb84841bc263d11c954e1c12b3c9e513e
Alexander <prepare4future@yahoo.com>
2019-10-24 10:11:33 +0400

    добавлен метод ортогонализации

--- main.js
+++ main.js
@@ -16,6 +16,82 @@ function parseMatrix() {
     return matrix;
 }
 
+function transMatrix(A) {
+    let m = A.length, n = A[0].length, AT = [];
+    for(let i = 0; i < n; i++) {
+        AT[i] = [];
+        for(let j = 0; j < m; j++) {
+            AT[i][j] = A[j][i];
+        }
+    }
+    return AT;
+}
+
+function multiplyMatrix(A, B) {
+    let rowsA = A.length, colsA = A[0].length,
+        rowsB = B.length, colsB = B[0].length,
+        C = [],
+        t = 0;
+    if(colsA !== rowsB) return false;
+    for(let i = 0; i < rowsA; i++) {
+        C[i] = [];
+    }
+    for(let k = 0; k < colsB; k++) {
+        for(let i = 0; i < rowsA; i++) {
+            t = 0;
+            for (let j = 0; j < rowsB; j++) {
+                t += A[i][j] * B[j][k];
+            }
+            C[i][k] = t;
+        }
+    }
+    return C;
+}
+
+function inverseMatrixDiagonal(matrix) {
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            if(i === j) {
+                matrix[i][j] = 1 / matrix[i][j];
+            }
+        }
+    }
+    return matrix;
+}
+
+function toSquare(matrix) {
+    let m = [];
+    for(let i = 0; i < matrix.length; i++) {
+        m[i] = [];
+        for(let j = 0; j < matrix.length; j++) {
+            m[i][j] = matrix[i][j];
+        }
+    }
+    return m;
+}
+
+function getBeta(matrix) {
+    let m = [];
+    for(let i = 0; i < matrix.length; i++) {
+        m[i] = [];
+        for(let j = matrix.length; j < matrix.length+1; j++) {
+            m[i][0] = matrix[i][j];
+        }
+    }
+    return m;
+}
+
+function toDiagonal(matrix) {
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            if(i !== j) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+
 function resulter(result, message) {
     let div = document.getElementById('result');
     div.innerHTML = '<p>' + message + '</p>';
--- ortogonalization.js
+++ ortogonalization.js
@@ -1,4 +1,38 @@
 function ortogonalization() {
     let matrix = parseMatrix();
+    let sum = 0;
+    let sqr = 0;
+    let lamba = 0;
+    let result = [];
+
+    for(let n = 0; n < matrix.length - 1; n++) {
+        for(let i = n+1; i < matrix.length; i++) {
+            sum = 0;
+            sqr = 0;
+            for(let j = 0; j < matrix.length; j++) {
+                sum += matrix[n][j] * matrix[i][j];
+                sqr += matrix[n][j] * matrix[n][j];
+            }
+            lamba = sum / sqr;
+            for(let j = 0; j < matrix.length + 1; j++) {
+                matrix[i][j] -= matrix[n][j] * lamba;
+            }
+        }
+    }
+
+    let beta = getBeta(matrix);
+    let r = toSquare(matrix);
+    let r_trans = transMatrix(r);
+    let d_reduce = multiplyMatrix(r, r_trans);
+    let d = toDiagonal(d_reduce);
+    let d_inverse = inverseMatrixDiagonal(d);
+    let x = multiplyMatrix(r_trans, d_inverse);
+    result = multiplyMatrix(x, beta);
+
+    for(let i = 0; i < result.length; i++) {
+        result[i] = Math.floor(result[i] * 100) / 100;
+    }
+
+    resulter(result, 'Метод ортогонализации');
 
 }
